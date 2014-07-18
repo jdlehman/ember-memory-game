671a4cbb72854a0ec5a04d8e65e34cb4a55e565c
Jonathan Lehman <jonathan.lehman91@gmail.com>
2014-07-17 20:19:33 -0400

    Only create a new game when data is submitted
    Fixes bug where games are created on every visit to index

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,30 +1,35 @@
 import Ember from 'ember';
 import ImageAdapter from 'ember-memory-game/adapters/image';
 
-export default Ember.ObjectController.extend({
+export default Ember.Controller.extend({
   category: '',
   actions: {
     newGame: function() {
       var category = this.get('category');
       if(category) {
         var controller = this;
+        // create game
+        var game = controller.store.createRecord('game', {
+          completed: false
+        });
+        // get images from google images
         var adapter = ImageAdapter.create();
         adapter.find(category).then(function(images) {
           images.map(function(image) {
+            // create card from image data
             var card = controller.store.createRecord('card', {
               image: image.imageName,
               selected: false,
               matched: false,
-              game: controller.get('model')
+              game: game
             });
-
-            // save card and associate to game
-            card.save().then(function(card) {
-              controller.get('model.cards').addObject(card);
+            // associate card to game
+            game.get('cards').then(function(cards) { // need this b/c cards assoc is async
+              cards.addObject(card);
             });
           });
           // transition to game route after adding all cards to game
-          controller.transitionToRoute('game', controller.get('model'));
+          controller.transitionToRoute('game', game);
           controller.set('category', '');
         });
       }
