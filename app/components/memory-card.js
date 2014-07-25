7cd6760990ee71446a550ebd14db610261e0d2c4
Jonathan Lehman <jonathan.lehman91@gmail.com>
2014-07-24 22:28:13 -0400

    Add selectable property on card component
    Prevent selected or matched cards from being selected again.
    Style selected cards.

--- app/components/memory-card.js
+++ app/components/memory-card.js
@@ -4,9 +4,14 @@ export default Ember.Component.extend({
   classNames: ['card'],
   classNameBindings: ['isSelected'],
   isSelected: Ember.computed.alias('selected'),
+  selectable: function() {
+    return !(this.get('selected') || this.get('matched'));
+  }.property('selected', 'matched'),
   actions: {
     select: function() {
-      this.toggleProperty('selected');
+      if(this.get('selectable')) {
+        this.toggleProperty('selected');
+      }
     }
   }
 });
